e4d55f51c9b53f634eefba7963bfbbe7bd90b6d4
MesClics <mesclics@hotmail.com>
2019-05-24 16:31:10 +0200

    use type hint dependency injection

--- Controller/CollectionController.php
+++ Controller/CollectionController.php
@@ -2,26 +2,45 @@
 
 namespace MesClics\PostBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Doctrine\ORM\EntityManagerInterface;
 use MesClics\PostBundle\Entity\Collection;
+use Symfony\Component\HttpFoundation\Request;
 use MesClics\PostBundle\Form\MesClicsCollectionType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use MesClics\PostBundle\Form\FormManager\CollectionFormManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Security("has_role('ROLE_EDITOR')")
  */
-class CollectionController extends Controller{
-    
-    public function collectionsAction(){
-        //on récupère la liste des collections déjà existantes
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('MesClicsPostBundle:Collection');
+class CollectionController extends Controller{    
+    private $collection_form_manager;
+    private $availbale_collections;
+    private $repo;
 
-        $collections = $repo->getCollections();
+    public function __construct(EntityManagerInterface $em, CollectionFormManager $collection_form_manager, $adminAvailableCollections){
+        $this->collection_form_manager = $collection_form_manager;
+        $this->available_collections = $adminAvailableCollections;
+        $this->repo = $em->getRepository('MesClicsPostBundle:Collection');
+    }
+
+    public function collectionsAction(Request $request){
+        //on récupère la liste des collections déjà existantes
+        $collections = $this->repo->getCollections();
 
         //on ajoute un formulaire pour l'ajout de collection
+        $options = array(
+            "available_collections" => $this->available_collections
+        );
+        $collection = new Collection();
+        $new_collection_form = $this->createForm(MesClicsCollectionType::class, $collection, $options);
+        if($request->isMethod('POST')){
+            $this->collection_form_manager->handle($new_collection_form);
+            if($this->collection_form_manager->hasSucceeded()){
+                return $this->redirectToRoute('mesclics_admin_collection', array('collection_id' => $collection->getId()));
+            }
+        }
         //on renvoie la vue permettant 'laffichage de la liste des collections
         $args = array(
             "currentSection" => "edition",
@@ -35,12 +54,15 @@ class CollectionController extends Controller{
         //on crée un objet collection
         $collection = new Collection();
         //on crée un formulaire
-        $options['available_collections'] = $this->container->getParameter('admin.collections');
+        $options['available_collections'] = $this->available_collections;
+
+        //on récupère les collections par type d'objets
+        $collections = $this->repo->getCollections();
 
         $form = $this->createForm(MesClicsCollectionType::class, $collection, $options);
         if($request->isMethod('POST')){
             //on initialise le form_manager
-            $form_manager = $this->get('mesclics_collection.form_manager.new');
+            $form_manager = $this->collection_form_manager;
             $form_manager->handle($form);
             if($form_manager->hasSucceeded()){
                 return $this->redirectToRoute('mesclics_admin_collections');
@@ -51,8 +73,34 @@ class CollectionController extends Controller{
             'currentSection' => 'edition',
             'subSection' => 'collections',
             'collectionSection' => 'new',
-            'new_collection_form' => $form->createView()
+            'new_collection_form' => $form->createView(),
+            'collections' => $collections
         );
+        return $this->render('MesClicsAdminBundle:Panel:edition.html.twig', $args);
+    }
+
+    
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     * @ParamConverter("collection", options={"mapping": {"collection_id": "id"}})
+     */
+    public function updateAction(Collection $collection, Request $request){
+        $options['available_collections'] = $this->available_collections;
+        $form = $this->createForm(MesClicsCollectionType::class, $collection, $options);
+        $args = array(
+            'currentSection' => 'edition',
+            'subSection' => 'collections',
+            'collectionSection' => 'edit',
+            'update_collection_form' => $form->createView()
+        );
+
+        if($request->isMethod('POST')){
+            $this->collectionFormManager->handle($form);
+            if($this->collectionFormManager->hasSucceeded()){
+                return $this->redirectToRoute('mesclics_admin_collection', array('collection_id' => $collection->getId()));
+            }
+        }
+
         return $this->render('MesClicsAdminBundle:Panel:edition.html.twig', $args);
     }
 }
