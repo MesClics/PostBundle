44adbd5bd42991f137f9568b5e7155c7b2e6970c
MesClics <mesclics@hotmail.com>
2019-10-29 17:17:29 +0100

    CLEAN + IMPROVEMENT :  create dispatch publicatiuon events own methods + add a custom flash message on turning a draft post into to be published or  published post

--- Event/Listener/MesClicsPostEventSubscriber.php
+++ Event/Listener/MesClicsPostEventSubscriber.php
@@ -2,6 +2,7 @@
 namespace MesClics\PostBundle\Event\Listener;
 
 use Psr\Log\LoggerInterface;
+use MesClics\PostBundle\Entity\Post;
 use MesClics\PostBundle\Event\MesClicsPostEvents;
 use MesClics\NavigationBundle\Navigator\Navigator;
 use MesClics\PostBundle\Actions\MesClicsPostActions;
@@ -45,16 +46,6 @@ class MesClicsPostEventSubscriber implements  EventSubscriberInterface{
     }
 
     public function onCreation(MesClicsPostCreationEvent $event){
-        $post = $event->getPost();
-        switch ($post->getVisibilite()){
-            case "public":
-            $visibilite = "publique";
-            break;
-
-            case "private":
-            $visibilite = "privée";
-            break;
-        }
         // add a flash message
         $message = $this->getCreationMessage($post);
 
@@ -67,49 +58,17 @@ class MesClicsPostEventSubscriber implements  EventSubscriberInterface{
     public function onUpdate(MesClicsPostUpdateEvent $event){
         $beforeUpdate = $event->getBeforeUpdate();
         $afterUpdate = $event->getAfterUpdate();
-        $beforeUpdateFilters = $beforeUpdate->getFilters();
-        $afterUpdateFilters = $afterUpdate->getFilters();
-        // dispatch publications Events
-        if($beforeUpdate->getDatePublication() !== $afterUpdate->getDatePublication()){
-            // publication date has changed
-            if(($beforeUpdate->isOnline() || $beforeUpdate->willBePublished()) && ($afterUpdate->isOnline() || $afterUpdate->willBePublished())){
-                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
-                $publicationEventName = MesClicsPostEvents::PUBLICATION;
-            }
-            //publication is now online
-            if(!$beforeUpdate->isOnline() && $afterUpdate->isOnline()){
-                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
-                $publicationEventName = MesClicsPostEvents::PUBLICATION;
-            }
-            //publication is now offline
-            if($beforeUpdate->isOnline() && !$afterUpdate->isOnline()){
-                $publicationEvent = new MesClicsPostDepublicationEvent($afterUpdate);
-                $publicationEventName = MesClicsPostEvents::DEPUBLICATION;
-            }
-            //TODO: publication is now draft
-            if(!$beforeUpdate->isDraft() && $afterUpdate->isDraft()){
-                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
-                $publicationEventName = MesClicsPostEvents::PUBLICATION;
-            }
-        }
 
-        if(($beforeUpdate->getDatePeremption() !== $afterUpdate->getDatePeremption())|| ($beforeUpdate->getDatePeremption() && !$afterUpdate->getDAtePeremption())){
-            $publicationEvent = new MesClicsPostDepublicationEvent($afterUpdate);
-            $publicationEventName = MesClicsPostEvents::DEPUBLICATION;
-        }
-
-        if($publicationEvent && $publicationEventName){
-            $this->event_dispatcher->dispatch($publicationEventName, $publicationEvent);
-        }
+        // eventually dispatch publication Events
+        self::dispatchPublicationEvents($beforeUpdate, $afterUpdate);
 
         // add a flash message for non publication dates updates
-        $this->addFlash('success', 'Votre publication ' . $event->getAfterUpdate()->getTitle() . ' a bien été modifiée.');
+        $this->addFlash('success', 'Votre publication ' . $afterUpdate->getTitle() . ' a bien été modifiée.');
 
 
         // add as action to navigator
         $action = MesClicsPostActions::update($afterUpdate);
         $this->navigator->getUser()->getChronology()->addAction($action);
-
     }
 
     public function onRemoval(MesClicsPostRemovalEvent $event){
@@ -121,8 +80,115 @@ class MesClicsPostEventSubscriber implements  EventSubscriberInterface{
     }
 
     public function onPublication(MesClicsPostPublicationEvent $event){
-        // add a flash message
         $post = $event->getPost();
+        // add a flash message
+        $message = $this->getPublicationMessage($post);
+        $this->addFlash('success', $message);
+    }
+
+    public function onDepublication(MesClicsPostDepublicationEvent $event){
+        $post = $event->getPost();
+        $message = $this->getDepublicationMessage($post);
+        // add a flash message
+        $this->addFlash('success', $message);
+    }
+
+    public function onCategorization(MesClicsPostCategorizationEvent $event){
+        // add a flash message
+        $type = $event->getCategorizationType();
+        $message = "";
+        
+        if($type["assigned_to"]){
+            $categories = array();
+            foreach($event->getNewCollections() as $cat){
+                $categories[] = $cat->getName();
+            }
+            $message .= "La publication " . $event->getPost()->getTitle() . " a bien été ajoutée aux collections suivantes : " . implode(", ", $categories) . '. ';
+        }
+
+        
+        if($type["removed_from"]){
+            $categories = array();
+            foreach($event->getOldCollections() as $cat){
+                $categories[] = $cat->getName();
+            }
+            $message .= "La publication " . $event->getPost()->getTitle() . " a bien été retirée des collections suivantes : " . implode(", ", $categories) . '. ';
+        }
+
+        $this->addFlash('success', $message);
+    }
+
+    private function dispatchPublicationEvents(Post $beforeUpdate, Post $afterUpdate){
+        if($beforeUpdate->getDatePublication() !== $afterUpdate->getDatePublication()){
+            // publication date has changed
+            if(($beforeUpdate->isOnline() || $beforeUpdate->willBePublished()) && ($afterUpdate->isOnline() || $afterUpdate->willBePublished())){
+                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
+                $publicationEventName = MesClicsPostEvents::PUBLICATION;
+            }
+            //publication is now online
+            if(!$beforeUpdate->isOnline() && ($afterUpdate->isOnline() || $afterUpdate->willBePublished())){
+                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
+                $publicationEventName = MesClicsPostEvents::PUBLICATION;
+            }
+            //publication is now offline
+            if($beforeUpdate->isOnline() && !$afterUpdate->isOnline()){
+                $publicationEvent = new MesClicsPostDepublicationEvent($afterUpdate);
+                $publicationEventName = MesClicsPostEvents::DEPUBLICATION;
+            }
+            //publication is now draft
+            if(!$beforeUpdate->isDraft() && $afterUpdate->isDraft()){
+                $publicationEvent = new MesClicsPostPublicationEvent($afterUpdate);
+                $publicationEventName = MesClicsPostEvents::PUBLICATION;
+            }
+        }
+
+        if(($beforeUpdate->getDatePeremption() !== $afterUpdate->getDatePeremption())|| ($beforeUpdate->getDatePeremption() && !$afterUpdate->getDAtePeremption())){
+            $publicationEvent = new MesClicsPostDepublicationEvent($afterUpdate);
+            $publicationEventName = MesClicsPostEvents::DEPUBLICATION;
+        }
+
+        if(isset($publicationEvent) && isset($publicationEventName)){
+            $this->event_dispatcher->dispatch($publicationEventName, $publicationEvent);
+        }
+    }
+
+    private function getCreationMessage(Post $post){
+        $post = $event->getPost();
+        switch ($post->getVisibilite()){
+            case "public":
+            $visibilite = "publique";
+            break;
+
+            case "private":
+            $visibilite = "privée";
+            break;
+        }
+        
+        if($post->isOnline()){
+            $message = "Votre nouvelle publication " . $visibilite . " a bien été publiée";
+             if($post->willBeUnpublished()){
+                 $message .= " et sera en ligne jusqu'au " . $post->getDatePeremption()->format('d/m/Y à H\hi');
+             }
+             $message .= ".";
+        } else{
+            $message = "Votre publication " . $visibilite . " a bien été enregistrée";
+            if($post->isDraft()){
+                $message .= " en tant que brouillon";
+            }
+            if($post->willBePublished()){
+                if($post->willBeUnpublished()){
+                    $publicationDates = " du " . $post->getDatePublication()->format('d/m/Y à H\hi') . " au " . $post->getDateDepublication()->format('d/m/Y à H\hi');
+                } else{
+                    $publicationDates = " le " . $post->getDatePublication()->format('d/m/Y à H\hi');
+                }
+                $message = " et sera publiée le " . $publicationDates;
+            }
+        }
+
+        return $message;
+    }
+
+    private function getPublicationMessage(Post $post){        
         switch ($post->getVisibilite()){
             case "public":
             $visibilite = "publique";
@@ -134,21 +200,21 @@ class MesClicsPostEventSubscriber implements  EventSubscriberInterface{
         }
 
         if($post->willBePublished()){
-            $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " sera publiée le " . $post->getDatePublication()->format("d/m/Y à H\i") . ".";
+            $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " sera publiée le " . $post->getDatePublication()->format("d/m/Y à H\hi") . ".";
         }
 
         if($post->isOnline()){
-            $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " est publiée.";
+            $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " est publiée depuis le " . $post->getDatePublication()->format("d/m/Y à H\hi") . ".";
         }
 
         if($post->isDraft()){
             $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " est enregistrée comme brouillon.";
         }
-        $this->addFlash('success', $message);
+
+        return $message;
     }
 
-    public function onDepublication(MesClicsPostDepublicationEvent $event){
-        $post = $event->getPost();
+    private function getDepublicationMessage(Post $post){        
         switch ($post->getVisibilite()){
             case "public":
             $visibilite = "publique";
@@ -181,58 +247,6 @@ class MesClicsPostEventSubscriber implements  EventSubscriberInterface{
 
         if(!$post->getDatePeremption()){
             $message = "Votre publication " . $visibilite . " " . $post->getTitle() . " n'a plus de date de dépublication.";
-        }
-
-        // add a flash message
-        $this->addFlash('success', $message);
-    }
-
-    public function onCategorization(MesClicsPostCategorizationEvent $event){
-        // add a flash message
-        $type = $event->getCategorizationType();
-        $message = "";
-        
-        if($type["assigned_to"]){
-            $categories = array();
-            foreach($event->getNewCollections() as $cat){
-                $categories[] = $cat->getName();
-            }
-            $message .= "La publication " . $event->getPost()->getTitle() . " a bien été ajoutée aux collections suivantes : " . implode(", ", $categories) . '. ';
-        }
-
-        
-        if($type["removed_from"]){
-            $categories = array();
-            foreach($event->getOldCollections() as $cat){
-                $categories[] = $cat->getName();
-            }
-            $message .= "La publication " . $event->getPost()->getTitle() . " a bien été retirée des collections suivantes : " . implode(", ", $categories) . '. ';
-        }
-
-        $this->addFlash('success', $message);
-    }
-
-    private function getCreationMessage(Post $post){
-        
-        if($post->isOnline()){
-            $message = "Votre nouvelle publication " . $visibilite . " a bien été publiée";
-             if($post->willBeUnpublished()){
-                 $message .= " et sera en ligne jusqu'au " . $post->getDatePeremption()->format('d/m/Y à H\hi');
-             }
-             $message .= ".";
-        } else{
-            $message = "Votre publication " . $visibilite . " a bien été enregistrée";
-            if($post->isDraft()){
-                $message .= " en tant que brouillon";
-            }
-            if($post->willBePublished()){
-                if($post->willBeUnpublished()){
-                    $publicationDates = " du " . $post->getDatePublication()->format('d/m/Y à H\hi') . " au " . $post->getDateDepublication()->format('d/m/Y à H\hi');
-                } else{
-                    $publicationDates = " le " . $post->getDatePublication()->format('d/m/Y à H\hi');
-                }
-                $message = " et sera publiée le " . $publicationDates;
-            }
         }
 
         return $message;
